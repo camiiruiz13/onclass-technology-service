0855e635a52fc2fecc48c1df4744a21ea541447d
camiiruiz13 <69661273+camiiruiz13@users.noreply.github.com>
2025-08-11 11:46:13 -0500

    Creacion de proyecto java spring webflux para reto de onclass-technology

---
+++ src/main/java/com/onclass/ccamilo/com/app/technologyservice/TechnologyserviceApplication.java
@@ -0,0 +1,13 @@
+package com.onclass.ccamilo.com.app.technologyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnologyserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnologyserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onclass/ccamilo/com/app/technologyservice/TechnologyserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onclass.ccamilo.com.app.technologyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnologyserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
